ce0262798472f1192076b9bffb44330528b1a22d
prakashsrirangaswamy <rangaswamy.prakash@gmail.com>
2018-08-05 21:24:38 -0500

    updated DAO class to latter add it to DB

---
+++ restAPI/src/main/java/org/prakash/rest/restAPI/DAO/CountyDetailsDAO.java
@@ -0,0 +1,21 @@
+package org.prakash.rest.restAPI.DAO;
+
+public class CountyDetailsDAO {
+	
+	int zipCode; 
+	String localities;
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getLocalities() {
+		return localities;
+	}
+	public void setLocalities(String localities) {
+		this.localities = localities;
+	} 
+	
+
+}
--- restAPI/src/main/java/org/prakash/rest/restAPI/DAO/GoogleAPIDAO.java
+++ restAPI/src/main/java/org/prakash/rest/restAPI/DAO/GoogleAPIDAO.java
@@ -1,28 +1,65 @@
 package org.prakash.rest.restAPI.DAO;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class GoogleAPIDAO {
 	int id;
-	String pincode;
+	String place_id;
+	int zipCode;
 	String location;
+	Calendar timestamp; 
+	String latitude;
+	String longitude;
+	List<CountyDetailsDAO> countyDetails; 
+	
+	public List<CountyDetailsDAO> getCountyDetails() {
+		return countyDetails;
+	}
+	public void setCountyDetails(List<CountyDetailsDAO> countyDetails) {
+		this.countyDetails = countyDetails;
+	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getPincode() {
-		return pincode;
+	public String getPlace_id() {
+		return place_id;
 	}
-	public void setPincode(String pincode) {
-		this.pincode = pincode;
+	public void setPlace_id(String place_id) {
+		this.place_id = place_id;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
 	}
 	public String getLocation() {
 		return location;
 	}
 	public void setLocation(String location) {
 		this.location = location;
+	}
+	public Calendar getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Calendar timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
 	} 
-	
-	
 	
 }
