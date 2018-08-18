58b74c7c250983b63fb3d16e5334c0308cc4e0cb
prakashsrirangaswamy <rangaswamy.prakash@gmail.com>
2018-08-17 21:59:32 -0500

    Mongo DB insert update

--- restAPI/src/main/java/org/prakash/rest/services/GoogleMapsServices.java
+++ restAPI/src/main/java/org/prakash/rest/services/GoogleMapsServices.java
@@ -1,13 +1,27 @@
 package org.prakash.rest.services;
 
-import org.prakash.rest.restAPI.Objects.MapsResponseObj;
+import org.bson.Document;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+@SpringBootApplication
 public class GoogleMapsServices {
 
-	
 
-	public void addDatatoDB(MapsResponseObj mapResObj) {
-		// TODO Auto-generated method stub
+
+	public void addDatatoDB(int zipcode, String formatted_address) {
+		MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017");
+		MongoClient client = new MongoClient(uri); 
+		MongoDatabase mdb = client.getDatabase("test"); 
+		MongoCollection<Document> collection = mdb.getCollection("googgleAPI");
+		Document doc = new Document(); 
+		doc.append("Zipcode", zipcode).append("Address", formatted_address); 
+		collection.insertOne(doc);
+		
 		
 	}
 
