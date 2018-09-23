88f293659a6a3497108bca2602ffb6d70b8a7eb6
prakashsrirangaswamy <rangaswamy.prakash@gmail.com>
2018-09-22 22:47:00 -0500

    updated collections

--- restAPI/src/main/java/org/prakash/rest/services/GoogleMapsServices.java
+++ restAPI/src/main/java/org/prakash/rest/services/GoogleMapsServices.java
@@ -16,11 +16,12 @@ import com.mongodb.client.MongoDatabase;
 public class GoogleMapsServices{
 
 	static final String URI = "mongodb://localhost:27017"; 
+	static final String Collection = "test"; 
 
 	public static void addDatatoDB(int zipcode, String formatted_address) {
 		MongoClientURI uri = new MongoClientURI(URI);
 		MongoClient client = new MongoClient(uri); 
-		MongoDatabase mdb = client.getDatabase("test"); 
+		MongoDatabase mdb = client.getDatabase(Collection); 
 		MongoCollection<Document> collection = mdb.getCollection("googgleAPI");
 		Document doc = new Document(); 
 		doc.append("Zipcode", zipcode).append("Address", formatted_address).append("Date", new Date()); 
