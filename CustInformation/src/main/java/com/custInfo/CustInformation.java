b8c18c762ae769d772df6955b714a21a191eb632
prakashsrirangaswamy <rangaswamy.prakash@gmail.com>
2019-07-02 10:29:33 +0530

    removed codes

--- CustInformation/src/main/java/com/custInfo/CustInformation.java
+++ CustInformation/src/main/java/com/custInfo/CustInformation.java
@@ -3,14 +3,11 @@
  */
 package com.custInfo;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,27 +20,22 @@ import com.custInfo.object.UserInformation;
 @Controller
 @RequestMapping("/CustInformation")
 public class CustInformation {
+
 	@Autowired
 	public CustInformation() {
 
 	}
 
-//	@RequestMapping(method = RequestMethod.GET)
-//	public String getCustInformation(ModelMap mm) {
-//		mm.addAttribute("displayName", mm.get("FName"));
-//		return "CustInformation.jsp";
-//	}
-	
 	@RequestMapping(method = RequestMethod.GET)
 	public String getCustInformation(HttpServletRequest req, HttpServletResponse res) {
-		
-		UserInformation ui = new UserInformation(); 
-		
-		ui.setFirstName((String)req.getParameter("FName"));
-		ui.setLastName((String)req.getParameter("LName"));
-		ui.setEmailID((String)req.getParameter("eMail"));
+
+		UserInformation ui = new UserInformation();
+
+		ui.setFirstName((String) req.getParameter("FName"));
+		ui.setLastName((String) req.getParameter("LName"));
+		ui.setEmailID((String) req.getParameter("eMail"));
 		System.out.println(ui.toString());
-		
+
 		return "CustInformation.jsp";
 	}
 
